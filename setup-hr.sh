e832d4845b76d5e479bf76c438572cc370b9c360
Eduard Bardají Puig <ebardaji@gmail.com>
2016-09-03 11:27:16 -0700

    starts new post hack reactor setup script

---
+++ setup-hr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#start at home directory
+cd
+
+
+#cleaning home directory after Ubuntu default install
+rm -f examples.desktop
+
+#creating dotfiles symbolics
+git clone https://github.com/aryzing/dotfiles.git
+ln -sb dotfiles/.gitconfig .
+ln -sb dotfiles/.git-prompt.sh .
+ln -sb dotfiles/.profile .
+ln -sb dotfiles/.vimrc .
+ln -sb dotfiles/.tmux.config .
